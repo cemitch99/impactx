e2126c0f08b64b77daa3ef1b992c933098a5a3a6
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2026-07-02 09:27:25 -0700

    Remove debug print statement.

--- src/particles/spacecharge/Gauss2p5dPush.cpp
+++ src/particles/spacecharge/Gauss2p5dPush.cpp
@@ -205,7 +205,6 @@ namespace impactx::particles::spacecharge
         bool apply_longitudinal_kick = true;
         pp_algo.queryAdd("apply_longitudinal_kick", apply_longitudinal_kick);
 
-        amrex::Print() << "apply_longitudinal_kick = " << apply_longitudinal_kick << "\n";
 
         // Measure beam size, extract the min, max of particle positions
         [[maybe_unused]] auto const [x_min, y_min, t_min, x_max, y_max, t_max] =
