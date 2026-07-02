a411ddbcd5b094568b299dbede4d2aafe10f748e
Chad Mitchell <46825199+cemitch99@users.noreply.github.com>
2026-07-02 11:50:56 -0700

    Restore computation of zi in analysis script.

--- examples/space_charge_gaussian_kick/analysis_sc_kick_Gauss2p5D_long_kick_off.py
+++ examples/space_charge_gaussian_kick/analysis_sc_kick_Gauss2p5D_long_kick_off.py
@@ -57,6 +57,7 @@ pyi = initial_sort["momentum_y"]
 ti = initial_sort["position_t"]
 pti = initial_sort["momentum_t"]
 zi = -beta * ti
+zi = -beta * ti
 
 # Predicted momentum kick, from J. Qiang, Phys. Rev. Accel. Beams 28, 114602 (2025), eqs. (31-32)
 
