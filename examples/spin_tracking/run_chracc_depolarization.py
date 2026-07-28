698ff993316c061715c5cbb2a9e7818e2ab5e717
Chad Mitchell <ChadMitchell@lbl.gov>
2026-07-28 12:45:47 -0700

    Update depolarization test.

--- examples/spin_tracking/run_chracc_depolarization.py
+++ examples/spin_tracking/run_chracc_depolarization.py
@@ -21,9 +21,9 @@ sim.slice_step_diagnostics = True
 sim.init_grids()
 
 # basic beam parameters
-kin_energy_MeV = 2000.0  # reference energy (kinetic)
+kin_energy_MeV = 20.00  # reference energy (kinetic)
 bunch_charge_C = 25.0e-12  # used with space charge
-npart = 100000  # number of macro particles
+npart = 1000000  # number of macro particles
 
 # set reference particle
 ref = sim.beam.ref
@@ -33,12 +33,12 @@ ref.set_species("proton").set_kin_energy_MeV(kin_energy_MeV)
 sigmaX = 0.003
 sigmaY = sigmaX
 sigmaPx = 0.0
-sigmaPy = 0.02
+sigmaPy = 0.2
 sigmaT = 0.0
 
-P1 = 0.4
-P2 = 0.9
-P3 = 0.1
+polarization_x = 0.4
+polarization_y = 0.9
+polarization_z = 0.1
 
 distr = distribution.Gaussian(
     lambdaX=sigmaX,
@@ -52,15 +52,15 @@ distr = distribution.Gaussian(
     mutpt=0.0,
 )
 spin = distribution.SpinvMF(
-    P1,
-    P2,
-    P3,
+    polarization_x,
+    polarization_y,
+    polarization_z,
 )
 
 sim.add_particles(bunch_charge_C, distr, npart, spin)
 
 # design the accelerator lattice
-ns = 10  # number of slices per ds in the element
+ns = 1  # number of slices per ds in the element
 
 # add beam diagnostics
 monitor = elements.BeamMonitor("monitor", backend="h5")
@@ -68,20 +68,13 @@ monitor = elements.BeamMonitor("monitor", backend="h5")
 gamma = ref.gamma
 beta_gamma = ref.beta_gamma
 
-ks_value = 1.0e-7
+#ks_value = 1.0e-7
+ks_value = 1.0e-8
+#ks_value = 0.0
 bz_value = ks_value * beta_gamma
 
 ez_value = 10.0
 ds_value = 50.0
-
-gamma_f = gamma + ez_value * ds_value
-beta_gamma_f = np.sqrt(gamma_f**2 - 1.0)
-mu = 0.5 * (beta_gamma_f / gamma_f - beta_gamma / gamma)
-lambda1 = 2.0 * mu * sigmaPy * beta_gamma
-
-P1f_pred = P1
-P2f_pred = P2 * np.exp(-(lambda1**2) / 2.0)
-P3f_pred = P3 * np.exp(-(lambda1**2) / 2.0)
 
 sol_chr = elements.ChrAcc(
     name="sol_chr", ds=ds_value, ez=ez_value, bz=bz_value, nslice=ns
@@ -95,5 +88,42 @@ sim.lattice.append(monitor)
 # run simulation
 sim.track_particles()
 
+gamma_f = ref.gamma
+beta_gamma_f = ref.beta_gamma
+
+G = ref.gyromagnetic_anomaly
+
+muterm = beta_gamma/(1+gamma) - beta_gamma_f/(1+gamma_f)
+Gterm = -G * np.log((beta_gamma_f+gamma_f)/(beta_gamma+gamma))
+lambda1 = np.abs(muterm + Gterm) * sigmaPy * beta_gamma
+
+polarization_xf_pred = polarization_x
+polarization_yf_pred = polarization_y * np.exp(-lambda1**2/2.0)
+polarization_zf_pred = polarization_z * np.exp(-lambda1**2/2.0)
+
+rbc = sim.beam.beam_moments()
+polarization_xf = rbc["mean_sx"]
+polarization_yf = rbc["mean_sy"]
+polarization_zf = rbc["mean_sz"]
+
+print("")
+print("Final polarization predicted:")
+print(
+    f"  polarization_x={polarization_xf_pred:e} polarization_y={polarization_yf_pred:e} polarization_z={polarization_zf_pred:e}"
+)
+print("")
+print("Final polarization:")
+print(
+    f"  polarization_x={polarization_xf:e} polarization_y={polarization_yf:e} polarization_z={polarization_zf:e}"
+)
+
 # clean shutdown
 sim.finalize()
+
+atol = 1.0e-3
+assert np.allclose(
+    [polarization_xf_pred, polarization_yf_pred, polarization_zf_pred],
+    [polarization_xf, polarization_yf, polarization_zf],
+    atol=atol,
+)
+
